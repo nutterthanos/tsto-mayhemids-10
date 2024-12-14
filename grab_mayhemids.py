fbadbdbce7cab3e611b49f9344c515cf6011ee12
GitHub Action <action@github.com>
2024-12-14 11:33:06 +0000

    Updated mayhemid's listings as of 2024-12-14 11:33:06 UTC

--- grab_mayhemids.py
+++ grab_mayhemids.py
@@ -7,8 +7,8 @@ from aiohttp import ClientResponseError, ClientPayloadError, ClientConnectorErro
 from xml.etree import ElementTree as ET
 
 # Define the range for applicationUserId
-START_ID = 10768000000
-END_ID = 10769000000
+START_ID = 10769000000
+END_ID = 10770000000
 
 # Maximum concurrent requests and retries
 MAX_CONCURRENT_REQUESTS = 25000
